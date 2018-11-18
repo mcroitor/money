7257bf5b6a3462abbb05f6bc4e0a43e562c0c7bb
Михаил Кройтор <m_croitor@hotmail.com>
2018-11-18 13:50:31 +0200

    money library

---
+++ money/money.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   money.h
+ * Author: Sunny
+ *
+ * Created on August 14, 2018, 7:25 AM
+ */
+
+#ifndef MONEY_H
+#define MONEY_H
+
+namespace mc {
+    class money{
+        size_t integral;
+        char part;
+        currency_t currency;
+    public:
+    };
+}
+
+#endif /* MONEY_H */
+
